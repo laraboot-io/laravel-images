2bceff51d8bcfca669ed95de4cb84385bc9f9204
Oscar Nevarez <fu.wire@gmail.com>
2020-12-10 11:31:40 -0600

    Include missing dependency

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -22,9 +22,14 @@ composer_cmd() {
 }
 
 require_dev(){
+  # Used to build several pieces of the foundation project
   composer_cmd require --dev laravel-shift/blueprint
+  # Used to replace configuration values via CLI
   composer_cmd require --dev oscarnevarezleal/laravel-sed:dev-dev
+  # Used to tests the generated code
   composer_cmd require --dev jasonmccreary/laravel-test-assertions
+  # Used to include cloud-ready capabilities in the generated project
+  composer_cmd require bref/laravel-bridge
 }
 
 create_breeze_setup() {
